41dfd999fe2749eb1d37701075c6317c01dcc51b
mactung <55038228+mactung@users.noreply.github.com>
2019-09-13 15:17:16 +0700

    initial commit

---
+++ Session 3/homework/li thuyet .js
@@ -0,0 +1,4 @@
+1. trong 1 mảng có chứa nhiều mảng khác gọi là array lồng
+ci dụ 
+let menu =[1,2,3,[5,6,7],8,9];
+2. Trong 1 mảng cũng có thể chứa nhiều kiểu dữ liệu. Nên có thể vừa có int vừa có string 
